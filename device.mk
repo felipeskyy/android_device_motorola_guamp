f78c5c65ca529e6f185f5888263022c1b1d6c057
Felipe <164790527+felipeskyy@users.noreply.github.com>
2024-11-14 20:18:45 +0000

    Adapting the tree to compile DerpFest

--- device.mk
+++ device.mk
@@ -32,7 +32,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-derp
 
 # Permissions
 PRODUCT_COPY_FILES += \
